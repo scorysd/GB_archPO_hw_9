0223a6c0b0cf8e112d27fb2c83cdcadfcc916271
Roman <toolatet@gmail.com>
2023-09-29 13:39:12 +0700

    init

---
+++ MyForstWebApplication/MyForstWebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyForstWebApplication.Models;
+
+namespace MyForstWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private WeatherForecastHolder _weatherForecastHolder;
+
+        public WeatherForecastController(
+            WeatherForecastHolder weatherForecastHolder)
+        {
+            _weatherForecastHolder = weatherForecastHolder;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add([FromQuery]  DateTime date, [FromQuery] int temperatureC)
+        {
+            _weatherForecastHolder.Add(date, temperatureC); 
+            return Ok();
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] DateTime date, [FromQuery] int temperatureC)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime date)
+        {
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public IActionResult Get([FromQuery] DateTime dateFrom, [FromQuery] DateTime dateTo)
+        {
+            List<WeatherForecast> list = _weatherForecastHolder.Get(dateFrom, dateTo);
+            return Ok(list);
+        }
+
+
+    }
+}
